b24e70dc110833bcf93b8a04267f1c4240eb0b3c
Ahmed Ayad <112045543+Ahmedayad11@users.noreply.github.com>
2022-08-27 01:35:50 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let img=document.querySelector(".img")
+let container=document.querySelector(".container");
+function phones(phones){
+    img.src=phones;
+}
+function colors(color){
+   container.style.background = color; 
+}
